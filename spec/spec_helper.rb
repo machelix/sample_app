c3a67f025029156fde538d00239d6ad980a480ca
Rajesh Huria <rajesh.huria@gmail.com>
2013-10-27 16:43:52 +0100

    More tests added for auth

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 require 'rubygems'
-require 'spork'
+#require 'spork'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
 
+=begin
 Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
@@ -55,6 +56,8 @@ Spork.each_run do
   # This code will be run each time you run your specs.
 
 end
+
+=end
 
 # --- Instructions ---
 # Sort the contents of this file into a Spork.prefork and a Spork.each_run
